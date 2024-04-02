ff8cb996f64f18f450ddd28e1e0f0d14f5b62152
fahadkabali <kabalifahad@gmail.com>
2024-04-02 23:38:32 +0300

    update

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,7 +4,7 @@ import Nav from "@components/Nav";
 import Provider from "@components/Provider";
 
 export const metadata = {
-  title: "Prompt",
+  title: "Spur",
   description: "Discover & Share AI Prompts",
 };
 
